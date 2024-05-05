e379bbded61300060528bee26bcd8d0585ed0f74
riyaraghuwanshi31 <127579773+riyaraghuwanshi31@users.noreply.github.com>
2024-05-05 17:59:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: ["https://my-portfolio-mern-taupe.vercel.app/"],
     methods: ["POST"],
     credentials: true,
   })
